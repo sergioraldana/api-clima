f1e2eb828e396b6a1beb1ea98d263d521f83f0e2
Sergio Renato Aldana Alvarez <sergio.aldana@galileo.edu>
2021-03-13 15:49:13 -0600

    final version

--- api-clima.js
+++ api-clima.js
@@ -1,33 +1,39 @@
 const climas = ['despejado', 'nublado', 'lluvioso'];
 
 
-const aleatorio = (min, max) => {
+function aleatorio (min, max) {
         min = Math.ceil(min);
         max = Math.floor(max);
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
 
-const obtenerClima = new Promise((resolve, reject) => {
+function obtenerClima(){
+    return new Promise((resolve, reject) => {
         let clima = '';
         clima = climas[aleatorio(0,2)]
+        console.log(clima);
         setTimeout(
             () => clima != ''
                 ? resolve(clima)
                 : reject(new Error('no encontrado')),
-            3000
+            1000
         );
     });
 
-    const obtenerTemperatura = new Promise((resolve, reject) => {
-        let temperatura;
-        temperatura = aleatorio(5,30);
-        setTimeout(
-            () => temperatura >= 5
-                ? resolve(temperatura)
-                : reject(new Error('no encontrado')),
-            3000
-        );
-    });
+}
+
+    function obtenerTemperatura() {
+        return new Promise((resolve, reject) => {
+            let temperatura;
+            temperatura = aleatorio(5,30);
+            setTimeout(
+                () => temperatura >= 5
+                    ? resolve(temperatura)
+                    : reject(new Error('no encontrado')),
+                1000
+            );
+        });
+    }
 
 module.exports = {
     obtenerClima,
